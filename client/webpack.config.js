44e8634faea4396e1ae499b90c08f2807a6beac3
Arjun Singh <arjunkhakh@gmail.com>
2022-05-14 17:02:42 +0100

    putting in the plugins, rules and commenting the web package page'

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,11 +1,10 @@
+// Requiring My Packages and Plugins
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+// Exporting the web packages for the application
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,15 +17,43 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      // Getting the HTML Plugin to go to Index.html
       new HtmlWebpackPlugin({
         template: './index.html'
       }),
 
-      new WebpackPwaManifest(),
+      // Creating a Manifest.json File 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'jate',
+        short_name: 'jate',
+        description: 'Text Editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }
+        
+      ),
 
-      new InjectManifest(),
+      // Using our S-Worker file for this application
+      new InjectManifest(
+        {
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+        }
+      ),
     ],
 
+    // Getting the Rules for CSS, Images and Babel-Loader
     module: {
       rules: [
         {
