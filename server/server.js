96f3c2fb1210d150c53831469f4472cb3bb14b19
Arjun Singh <arjunkhakh@gmail.com>
2022-06-02 13:35:40 +0100

    deployment and debugging

--- server/server.js
+++ server/server.js
@@ -1,23 +1,12 @@
 const express = require('express');
-<<<<<<< HEAD
-const path = require('path')
-=======
-
->>>>>>> ce73710081bf607f4df864f70a1a8f0e21262eb0
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-<<<<<<< HEAD
 // console.log(path.join(__dirname, '../client/dist/index.html'))
 require('./routes/htmlRoutes')(app);
 
 
-=======
-
-require('./routes/htmlRoutes')(app);
-
->>>>>>> ce73710081bf607f4df864f70a1a8f0e21262eb0
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
